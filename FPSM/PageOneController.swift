2ee310cb490f45a199954913ae670cb6c49816d5
Steve <saodell@mac.com>
2017-06-12 14:50:20 -0600

    Rebuilt PageOneViewController without background images

--- FPSM/PageOneController.swift
+++ FPSM/PageOneController.swift
@@ -14,30 +14,71 @@ import KeychainSwift
 class PageOneController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     //Mark: Properties
-
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var childNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
 
     }
     
     //MARK: UITextFieldDelegate
-    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //Hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
 
-    
-
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        childNameLabel.text = textField.text
+    }
     
     //MARK: UIImagePickerControllerDelegate
     
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
+    }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        // Set photoImageView to display the selected image.
+        photoImageView.image = selectedImage
+        
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
     
     //Mark: Actions
-
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        
+        //Hide the keyboard
+        nameTextField.resignFirstResponder()
+        
+        //Let user pick media from photo library
+        let imagePickerController = UIImagePickerController()
+        
+        // Only allow photos to be picked, not taken.
+        imagePickerController.sourceType = .photoLibrary
+        
+        //ViewController is notified that user picks an image
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
     
-
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        childNameLabel.text = "Default Text"
+    }
 
     // Sign out actions
     @IBAction func SignOut (_ sender: Any) {
