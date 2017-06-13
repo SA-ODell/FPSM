2af22470ee3cbe5d3410b35c6d1b34d54546e018
Steve <saodell@mac.com>
2017-06-13 10:18:43 -0600

    Initialize class creation

---
+++ KidClassControll.swift
@@ -0,0 +1,21 @@
+//
+//  KidClassControll.swift
+//  
+//
+//  Created by Steve @work on 6/13/17.
+//
+//
+
+import UIKit
+
+class KidClassControll: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
